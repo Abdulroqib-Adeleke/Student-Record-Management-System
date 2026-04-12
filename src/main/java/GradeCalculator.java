f0897df3b2305b628432e87b492033e49e295bbc
Abdulroqib-Adeleke <adelekeabdulroqib29@gmail.com>
2026-04-12 14:03:51 +0100

    Created a method that converts course score into grade

--- src/main/java/GradeCalculator.java
+++ src/main/java/GradeCalculator.java
@@ -1,2 +1,17 @@
 public class GradeCalculator {
+
+    public void gradeConverter(Course course){
+
+        if(course.getGrade() >= 80){
+
+        }else if (course.getGrade() >= 70){
+
+        }else if (course.getGrade() >= 60){
+
+        }else if (course.getGrade() >= 50){
+
+        }else if (course.getGrade() >= 40){
+
+        }
+    }
 }
