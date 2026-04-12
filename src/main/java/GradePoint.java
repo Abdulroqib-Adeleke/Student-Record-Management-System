8136fac76bcf6ad4777ea5e71946a78a96cc9bcc
Abdulroqib-Adeleke <adelekeabdulroqib29@gmail.com>
2026-04-12 14:15:17 +0100

    Updated grade point method

--- src/main/java/GradePoint.java
+++ src/main/java/GradePoint.java
@@ -6,20 +6,17 @@ public class GradePoint {
     public int unitScore(Course course){
 
         int unit = course.getCourseUnit();
-        int result = 0;
-        if(course.getGrade() >= 80){
-            result = 5;
-        } else if (course.getGrade() >= 70) {
-            result = 4;
-        } else if (course.getGrade() >= 60) {
-            result = 3;
-        } else if (course.getGrade() >= 50) {
-            result = 2;
-        } else if (course.getGrade() >= 40) {
-            result = 1;
-        }else {
-            result = 0;
-        }
+
+        int result = switch (course.getGrade()) {
+            case 'A' -> 5;
+            case 'B' -> 4;
+            case 'C' -> 3;
+            case 'D' -> 2;
+            case 'E' -> 1;
+            case 'F' -> 0;
+            default -> 0;
+        };
+
         return result * unit;
     }
 }
