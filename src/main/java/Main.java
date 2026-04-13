82fc55161020279c75e944b1be3b1737b4dbd6b2
Abdulroqib-Adeleke <adelekeabdulroqib29@gmail.com>
2026-04-13 14:14:51 +0100

    Added a start and function method in UI class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,9 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        Course c1 = new Course("java101", "intro java", 5, 95);
-        Course c2 = new Course("Spring102", "intro Spring", 3, 77);
+        UserInterface ui = new UserInterface();
 
-        Student stud = new Student("AY", 101, 18, "CS");
-        stud.setOfferedCourse(c1);
-        stud.setOfferedCourse(c2);
-        stud.printGPA();
+        ui.start();
 
     }
 }
--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,2 +1,41 @@
+import java.util.Scanner;
+
 public class UserInterface {
+
+    Scanner scan = new Scanner(System.in);
+    Student student = new Student();
+    Course course;
+
+    public void start(){
+        System.out.println("Welcome to Student record management portal");
+
+        while (true){
+            System.out.println("Select a function\n" +
+                    "1. Enroll a student\n" +
+                    "2. Add course");
+
+            int input = scan.nextInt();
+
+            functions(input);
+            break;
+        }
+
+    }
+
+    public void functions(int input){
+        if (input == 1){
+            System.out.println("Enter student name");
+            String name = scan.nextLine();
+            student.setName(name);
+        } else if (input == 2) {
+            System.out.println("Enter course code");
+            String courseCode = scan.nextLine();
+            System.out.println("Enter course title");
+            String courseTitle = scan.nextLine();
+            System.out.println("Enter course unit");
+            int courseUnit = scan.nextInt();
+
+            course = new Course(courseCode, courseTitle, courseUnit);
+        }
+    }
 }
